46484c83ebdb2ef85c4233b117be9479d8f607f7
Laura Pigosso <127753547+LauraPigosso@users.noreply.github.com>
2023-05-22 16:44:30 -0300

    Update janela.py

--- janela.py
+++ janela.py
@@ -2,20 +2,27 @@ from tkinter import *
 from conexao import conexao, cursor
 from tkinter import ttk
 
-janela = Tk()
 
 
 class Janela:
     def __init__(self):
-        self.janela = janela
+        self.janela = Tk()
         self.tela()
+        self.janela.mainloop()
+        self.frames()
 
     def tela(self):
         self.janela.title('Magalu')
-        self.janela.configure(background='#bdbcb3')
+        self.janela.configure(background='#fff')
         self.janela.geometry('700x500')
         self.janela.resizable(True, True)
         self.janela.maxsize(width=700, height=500)
+
+    def frames(self):
+        self.frame0 = Frame(self.janela, bg='#E8AEB7')
+        self.frame0.place(relx=0.03, rely=0.03, relwidth=0.94, relheight=0.11)
+
+
 
 
 Janela()
